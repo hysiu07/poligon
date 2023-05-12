bc388d7cffb93a09414b3f1c55491b88801e3a03
Daniel <hysiu07@gmail.com>
2023-05-12 19:59:08 +0200

    only testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+const styles = {
+	backgroundColor: 'gray',
+	border: 'lime 1px solid',
+	padding: 10,
+	color: {
+		color: 'red',
+	},
+};
+
+// function News(props) {
+// 	const { header, intro } = props;
+
+// 	return (
+// 		<div style={styles}>
+// 			<h2>{header}</h2>
+// 			<p style={styles.color}>{intro}</p>
+// 		</div>
+// 	);
+// }
+
+class News extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date(),
+			score: 5,
+		};
+	}
+	componentDidMount() {
+		this.setState({score:8})
+	}
+	render() {
+		const { header, intro } = this.props;
+		return (
+			<div style={styles}>
+				<h2>{header}</h2>
+				<p style={styles.color}>{intro}</p>
+				<p>{`Mój wynik to ${this.state.score}`}</p>
+				<p>{`Dzisiejsza data ${this.state.date}`}</p>
+			</div>
+		);
+	}
+}
+
+const data = [
+	{
+		header: 'nagłówek 1',
+		intro: 'Intro 1',
+	},
+	{
+		header: 'nagłówek 2',
+		intro: 'Intro 2',
+	},
+	{
+		header: 'nagłówek 3',
+		intro: 'Intro 3',
+	},
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			{data.map((el, index) => {
+				return <News key={index} header={el.header} intro={el.intro} />;
+			})}
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  const helloElement = screen.getByText(/hello/i);
   expect(linkElement).toBeInTheDocument();
+  expect(helloElement).toBeInTheDocument();
 });
